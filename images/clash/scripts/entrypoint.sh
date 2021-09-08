05c806e32eecbbb655f62d94daf18186d3d46138
root <fancl20@gmail.com>
2021-09-08 11:50:57 +1000

    Update entrypoint

--- images/clash/scripts/entrypoint.sh
+++ images/clash/scripts/entrypoint.sh
@@ -1,5 +1,16 @@
+#!/bin/sh
+
+# Create tun devixe
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 chmod 600 /dev/net/tun
+
+# Append proxies to the config
+if [ -f /vault/secrets/proxies ]; then
+  cat /vault/secrets/ >> /root/.config/clash/config.yaml
+fi
+
+# setup_tun.sh will wait until utun available and setup route table
+/root/scripts/setup_tun.sh &
 
 exec /opt/bin/clash
